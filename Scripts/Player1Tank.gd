b597aa776b347b2d6161b6285626d6666f7613f8
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-11-10 00:40:33 +0000

    feat: update inputs to the 2 player and add 2 player to the main scene
    #8

---
+++ Scripts/Player1Tank.gd
@@ -0,0 +1,16 @@
+extends "res://Scripts/Tank.gd"
+
+func _input_rotate() -> float:
+	var rotation_direction = 0
+	if Input.is_action_pressed("p1_rotate_left"):
+		rotation_direction -= 1
+	if Input.is_action_pressed("p1_rotate_right"):
+		rotation_direction += 1
+	return rotation_direction
+
+func _input_move() -> void:
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("p1_move_forward"):
+		velocity = Vector2(speed, 0)
+	if Input.is_action_pressed("p1_move_backwards"):
+		velocity = Vector2(-speed / 2, 0)
--- Scripts/Tank.gd
+++ Scripts/Tank.gd
@@ -4,34 +4,41 @@ extends KinematicBody2D
 export var speed :int = 200 
 
 # Speed to rotate the tank to the left/right
-export var rotation_speed :int = 200
+export var rotation_speed :int = 1.5
 
 # Delay in seconds between shoots
 export var shoot_delay :float = 1
 var current_shoot_delay :float = 0
+
+# "Private" variable to move the tank
+var velocity :Vector2 = Vector2.ZERO
 
 func _process(delta :float):
 	# Update timers	
 	current_shoot_delay = clamp(current_shoot_delay + delta, 0, shoot_delay)
 	
 	# Shoot
-	if Input.is_action_pressed("shoot") and current_shoot_delay == shoot_delay:
+	if Input.is_action_pressed("p1_shoot") and current_shoot_delay == shoot_delay:
 		print("Shoot") # TODO: Implement this action
 		current_shoot_delay = 0
 
 func _physics_process(delta :float):
-	# Update rotation
-	if Input.is_action_pressed("rotate_left"):
-		rotate(deg2rad(-rotation_speed) * delta)
-	if Input.is_action_pressed("rotate_right"):
-		rotate(deg2rad(rotation_speed) * delta)
+	# Rotate
+	var rotation_direction = _input_rotate()
+	rotation += rotation_direction * rotation_speed * delta
 	
-	# Calculate forward vector
-	var current_rotation := transform.get_rotation()
-	var forward_vector := Vector2.UP.rotated(current_rotation)
-	
-	# Update movement
-	if Input.is_action_pressed("move_forward"):
-		move_and_collide(forward_vector * rotation_speed * delta)
-	if Input.is_action_pressed("move_backwards"):
-		move_and_collide(-forward_vector * rotation_speed * delta)
+	# Move
+	_input_move()
+	velocity = velocity.rotated(rotation)
+	velocity = move_and_slide(velocity)
+	pass
+
+## Update rotation
+## This function must be override
+func _input_rotate() -> float:
+	return 0.0
+
+## Update movement
+## This function must be override
+func _input_move() -> void:
+	pass
