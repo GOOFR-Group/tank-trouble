579869e9e3b5640a35599f170e7ea976689d93cc
Goncalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-11-10 10:44:46 +0000

    feat: update tank script to spawn bullet on shoot

--- Scripts/Tank.gd
+++ Scripts/Tank.gd
@@ -1,5 +1,10 @@
 extends KinematicBody2D
 
+# Scenes
+onready var main_scene = get_tree().root
+onready var bullet_scene = preload("res://Prefabs/Bullet.tscn")
+onready var bullet_spawn_point = get_node("./BulletSpawnPoint")
+
 # Speed to move the tank forward/backwards 
 export var speed :int = 200 
 
@@ -7,19 +12,24 @@ export var speed :int = 200
 export var rotation_speed :int = 200
 
 # Delay in seconds between shoots
-export var shoot_delay :float = 1
+export var shoot_delay :float = 0.25
 var current_shoot_delay :float = 0
 
-func _process(delta :float):
-	# Update timers	
+func _process(delta :float) -> void:
+	# Update timer
 	current_shoot_delay = clamp(current_shoot_delay + delta, 0, shoot_delay)
 	
 	# Shoot
-	if Input.is_action_pressed("shoot") and current_shoot_delay == shoot_delay:
-		print("Shoot") # TODO: Implement this action
+	if Input.is_action_pressed("shoot") && current_shoot_delay == shoot_delay:
+		var bullet = bullet_scene.instance()
+		bullet.set_position(bullet_spawn_point.get_global_position())
+		bullet.set_rotation(bullet_spawn_point.get_global_rotation())
+		main_scene.add_child(bullet)
+		
+		# Reset timer
 		current_shoot_delay = 0
 
-func _physics_process(delta :float):
+func _physics_process(delta :float) -> void:
 	# Update rotation
 	if Input.is_action_pressed("rotate_left"):
 		rotate(deg2rad(-rotation_speed) * delta)
