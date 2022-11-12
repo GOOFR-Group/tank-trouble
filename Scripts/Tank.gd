99e244df2165717d0f27ef5ef23e9c0efd2e8a07
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2022-11-12 19:10:06 +0000

    fix: last commit

--- Scripts/Tank.gd
+++ Scripts/Tank.gd
@@ -1,24 +1,21 @@
 extends KinematicBody2D
 
-# Speed to move the tank forward/backwards 
-export var speed :int = 200 
+## Speed to move the tank forward/backwards 
+export var speed :float = 200 
 
-# Speed to rotate the tank to the left/right
-export var rotation_speed :int = 1.5
+## Speed to rotate the tank to the left/right
+export var rotation_speed :float = 1.5
 
-# Delay in seconds between shoots
+## Delay in seconds between shoots
 export var shoot_delay :float = 1
 var current_shoot_delay :float = 0
 
-# "Private" variable to move the tank
-var velocity :Vector2 = Vector2.ZERO
-
 func _process(delta :float):
-	# Update timers	
+	# Update timers
 	current_shoot_delay = clamp(current_shoot_delay + delta, 0, shoot_delay)
 	
 	# Shoot
-	if Input.is_action_pressed("p1_shoot") and current_shoot_delay == shoot_delay:
+	if Input.is_action_pressed("p1_shoot") && current_shoot_delay == shoot_delay:
 		print("Shoot") # TODO: Implement this action
 		current_shoot_delay = 0
 
@@ -28,17 +25,16 @@ func _physics_process(delta :float):
 	rotation += rotation_direction * rotation_speed * delta
 	
 	# Move
-	_input_move()
-	velocity = velocity.rotated(rotation)
-	velocity = move_and_slide(velocity)
-	pass
+	var forward_vector := Vector2.UP.rotated(rotation)
+	var move_direction = _input_move()
+	move_and_slide(move_direction * forward_vector * speed)
 
 ## Update rotation
 ## This function must be override
-func _input_rotate() -> float:
-	return 0.0
+func _input_rotate() -> int:
+	return 0
 
 ## Update movement
 ## This function must be override
-func _input_move() -> void:
-	pass
+func _input_move() -> int:
+	return 0
