3d2e31faa1092d99dc6ebb24c78d309b2d11830f
Goncalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-12-14 14:30:25 +0000

    feat: add possibility to change block color

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,9 +3,11 @@ extends Node
 signal game_over(playerkilled)
 
 var is_game_over :bool
+var debug :bool
 
 func _ready() -> void:
 	is_game_over = false
+	debug = true
 
 func game_over(player_killed: String) -> void:
 	is_game_over = true
@@ -17,3 +19,6 @@ func restart_game() -> void:
 	var error = get_tree().reload_current_scene()
 	if error != OK:
 		push_error("Failed to restart the game.")
+
+func toggle_debug_mode() -> void:
+	debug = !debug
