f177ab5530314df9ed4bf327580f79f155f48931
Goncalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2023-06-17 21:28:39 +0100

    feat: update restart menu

--- Scripts/UI/RestartMenu.gd
+++ Scripts/UI/RestartMenu.gd
@@ -13,12 +13,12 @@ func game_over(winner: String) -> void:
 	$Background/Label.text = winner + " won!"
 	$AnimationPlayer.play("show")
 
-func _on_restart_button_pressed() -> void:
-	GameManager.restart_game()
-
-func _on_exit_button_pressed() -> void:
+func _on_menu_button_pressed() -> void:
 	GameManager.is_game_over = false
 	
 	var error := get_tree().change_scene("res://Levels/MenuLevel.tscn")
 	if error != OK:
 		push_error("RestartMenu failed to change to Menu.")
+		
+func _on_restart_button_pressed() -> void:
+	GameManager.restart_game()
