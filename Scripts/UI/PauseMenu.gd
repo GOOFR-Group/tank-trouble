460755d9d8e5bdb9e34d9fcda8ccf4e1fe1054fb
Goncalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2023-06-17 23:38:38 +0100

    fix: do not pause when game is over

--- Scripts/UI/PauseMenu.gd
+++ Scripts/UI/PauseMenu.gd
@@ -1,19 +1,14 @@
 extends CanvasLayer
 
-func _ready() -> void:
-	var error := GameManager.connect("game_over", self, "game_over")
-	if error != OK:
-		push_error("PauseMenu failed to connect the game_over signal.")
-
 func _process(_delta: float) -> void:
+	if GameManager.is_game_over:
+		return
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		if get_tree().paused:
 			_hide_pause_menu()
 		else:
 			_show_pause_menu()
-
-func game_over(_winner: String) -> void:
-	hide()
 
 func _show_pause_menu() -> void:
 	get_tree().paused = true
